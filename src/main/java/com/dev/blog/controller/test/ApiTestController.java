4202bd25e658f06f3ea3f8aca87df7514b369761
devjjin <sujink3434@gmail.com>
2022-06-29 18:23:54 +0900

    IllegalException 처리

--- src/main/java/com/dev/blog/controller/test/ApiTestController.java
+++ src/main/java/com/dev/blog/controller/test/ApiTestController.java
@@ -13,8 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Supplier;
 
 @RestController
 public class ApiTestController {
