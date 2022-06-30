051beabc553506ccbe5ed331b8229654d1445601
devjjin <sujink3434@gmail.com>
2022-06-30 10:11:57 +0900

    로그인 시큐리티 적용 - 기존 로그인 로직 수정

--- src/main/java/com/dev/blog/repository/UserRepository.java
+++ src/main/java/com/dev/blog/repository/UserRepository.java
@@ -2,8 +2,6 @@ package com.dev.blog.repository;
 
 import com.dev.blog.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsernameAndPassword(String username, String password);
 }
